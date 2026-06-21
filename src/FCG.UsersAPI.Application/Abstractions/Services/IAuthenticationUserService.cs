830417451a1a491b68fb23115d29b91f9751e6d5
pedro <pedro@PEDROIGHORNOTEB>
2026-06-21 09:31:28 -0300

    feat: criacao das interfaces em repositories e services dentro da camada de Application.

---
+++ src/FCG.UsersAPI.Application/Abstractions/Services/IAuthenticationUserService.cs
@@ -0,0 +1,10 @@
+﻿using FCG.UsersAPI.Application.Contracts.Dto;
+using FCG.UsersAPI.Domain.Common;
+
+namespace FCG.UsersAPI.Application.Abstractions.Services
+{
+    public interface IAuthenticationUserService
+    {
+        Task<Result<string>> Login(UserForAuthenticationDTO userEntity);
+    }
+}
---
+++ src/FCG.UsersAPI.Application/Abstractions/Services/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using FCG.UsersAPI.Application.Contracts.Dto;
+using FCG.UsersAPI.Domain.Common;
+using FCG.UsersAPI.Domain.Entities;
+
+namespace FCG.UsersAPI.Application.Abstractions.Services
+{
+    public interface IUserService
+    {
+        Task<Result<UserEntity>> Add(CreateUserRequestDto createUserRequestDto);
+        Task<Result<bool>> Update(string id, UpdateUserRequestDto updateUserRequestDto);
+        Task<Result<bool>> Delete(string id);
+        Task<List<UserEntity>> Get();
+        Task<Result<UserResponseDto>> GetById(string id);
+    }
+}
