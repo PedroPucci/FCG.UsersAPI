ebf9f7c067db1de3b47d7de6f5fc491f7276ccb2
pedro <pedro@PEDROIGHORNOTEB>
2026-06-21 09:34:41 -0300

    feat: implementacao do ProfileConfiguration, DataContext e repositorio para usuario

---
+++ src/FCG.UsersAPI.Infrastructure/Configurations/ProfileConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using FCG.UsersAPI.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FCG.UsersAPI.Infrastructure.Configurations
+{
+    public class ProfileConfiguration : IEntityTypeConfiguration<ProfileEntity>
+    {
+        public void Configure(EntityTypeBuilder<ProfileEntity> builder)
+        {
+            builder.HasData(
+                new ProfileEntity
+                {
+                    Id = "0",
+                    Name = "Administrator",
+                    NormalizedName = "ADMINISTRATOR"
+                },
+                new ProfileEntity
+                {
+                    Id = "1",
+                    Name = "Usuario",
+                    NormalizedName = "USUARIO"
+                }
+            );
+        }
+    }
+}
---
+++ src/FCG.UsersAPI.Infrastructure/Repository/UserRepository.cs
@@ -0,0 +1,86 @@
+﻿using FCG.UsersAPI.Application.Abstractions.Repositories;
+using FCG.UsersAPI.Domain.Entities;
+using FCG.UsersAPI.Infrastructure.Connections;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace FCG.UsersAPI.Infrastructure.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DataContext _context;
+        private readonly UserManager<UserEntity> _userManager;
+
+        public UserRepository(
+            DataContext context,
+            UserManager<UserEntity> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task<UserEntity> Add(UserEntity userEntity)
+        {
+            var result = await _userManager.CreateAsync(userEntity, userEntity.PasswordHash);
+
+            if (!result.Succeeded)
+                throw new InvalidOperationException(
+                    string.Join(" | ", result.Errors.Select(e => e.Description)));
+
+            return userEntity;
+        }
+
+        public async Task<bool> CheckPassword(UserEntity userEntity, string password)
+        {
+            var result = await _userManager.CheckPasswordAsync(userEntity, password);
+            return result;
+        }
+
+        public async Task<bool> Delete(string id)
+        {
+            var user = await GetByIdCheck(id);
+
+            if (user == null)
+                return false;
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<List<UserEntity>> Get()
+        {
+            return await (
+                from user in _context.Users.AsNoTracking()
+                join userRole in _context.UserRoles.AsNoTracking()
+                    on user.Id equals userRole.UserId
+                join role in _context.Roles.AsNoTracking()
+                    on userRole.RoleId equals role.Id
+                orderby user.Id
+                select new UserEntity
+                {
+                    Email = user.Email,
+                    Name = user.Name,
+                    IsActive = user.IsActive
+                }
+            ).ToListAsync();
+        }
+
+        public async Task<UserEntity> GetByEmail(string email)
+        {
+            var result = await _userManager.FindByEmailAsync(email);
+            return result;
+        }
+
+        public async Task<UserEntity?> GetByIdCheck(string id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public UserEntity Update(UserEntity userEntity)
+        {
+            return _context.Users.Update(userEntity).Entity;
+        }
+    }
+}
